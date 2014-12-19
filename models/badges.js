f15e6129e5ac665ade9aa6a86361c358a26abec0
Jaosn Zhang <zhangyue1208@gmail.com>
2014-12-19 00:04:22 +0000

    finished pub/sub server part

---
+++ models/badges.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var redis = require('../lib/redis');
+var broadcast = require('../lib/broadcast');
+
+/**
+ *  save badges to database
+ *  @Params {Array} badges
+ *  @Params {Function} callback
+ */
+
+exports.save = function(badges, callback) {
+    if (!badges.length) return callback(null, null);
+    var badge = badges.pop();
+    redis.lpush('badges', JSON.stringify(badge), function(err) {
+        if (err) return callback(err, null);
+        exports.save(badges, callback);
+    });
+};
+
+/**
+ * Trim down the redis list
+ */
+
+exports.trim = function() {
+   redis.ltrim('badges', 0, 9);
+};
+
+/*
+ * Send out badges to the broadcaster
+ * @Params {Array} badges
+ * @Params {Function} callback
+ */
+
+exports.send = function(badges, callback) {
+    badges.forEach(broadcast.send);
+    callback(null, null);
+};
+
+/**
+ * Get badges from redis
+ */
+
+exports.get = function(callback) {
+    redis.lrange('badges', 0, -1, function(err, data) {
+        if (err) return callback(err, null);
+        callback(null, data.map(JSON.parse));
+    });
+};
